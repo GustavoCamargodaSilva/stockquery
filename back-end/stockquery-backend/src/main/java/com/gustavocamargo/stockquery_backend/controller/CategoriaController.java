f617dc28610146e453676e82f46377d0e26ca36d
GustavoCamargodaSilva <gustavocamargodasilva@hotmail.com>
2024-07-30 00:27:00 -0300

    feito injecao de dependencias spring

--- back-end/stockquery-backend/src/main/java/com/gustavocamargo/stockquery_backend/controller/CategoriaController.java
+++ back-end/stockquery-backend/src/main/java/com/gustavocamargo/stockquery_backend/controller/CategoriaController.java
@@ -1,9 +1,15 @@
 package com.gustavocamargo.stockquery_backend.controller;
 
+import com.gustavocamargo.stockquery_backend.entities.Categoria;
+import com.gustavocamargo.stockquery_backend.service.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaController {
+
+    @Autowired
+    CategoriaService categoriaService;
 }
--- back-end/stockquery-backend/src/main/java/com/gustavocamargo/stockquery_backend/service/CategoriaService.java
+++ back-end/stockquery-backend/src/main/java/com/gustavocamargo/stockquery_backend/service/CategoriaService.java
@@ -1,7 +1,12 @@
 package com.gustavocamargo.stockquery_backend.service;
 
+import com.gustavocamargo.stockquery_backend.repositorio.CategoriaRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CategoriaService {
+
+    @Autowired
+    CategoriaRepositorio repositorio;
 }
