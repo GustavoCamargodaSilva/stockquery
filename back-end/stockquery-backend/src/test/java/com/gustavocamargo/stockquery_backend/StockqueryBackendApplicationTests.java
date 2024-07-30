82552d479a5d3372fa34bfbd56a6cc7419e74726
GustavoCamargodaSilva <gustavocamargodasilva@hotmail.com>
2024-07-29 23:01:12 -0300

    arquivos padrao projeto

---
+++ back-end/stockquery-backend/src/test/java/com/gustavocamargo/stockquery_backend/StockqueryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavocamargo.stockquery_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockqueryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
