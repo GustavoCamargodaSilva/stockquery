dd29c8b632078d40cde974faca71b397904f82cb
GustavoCamargodaSilva <gustavocamargodasilva@hotmail.com>
2024-07-30 00:22:41 -0300

    criado controller e service sem nada

--- back-end/stockquery-backend/src/main/java/com/gustavocamargo/stockquery_backend/repositorio/CategoriaRepositorio.java
+++ back-end/stockquery-backend/src/main/java/com/gustavocamargo/stockquery_backend/repositorio/CategoriaRepositorio.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoriaRepositorio extends JpaRepository<Long, Categoria> {
+public interface CategoriaRepositorio extends JpaRepository<Categoria, Long> {
 
 }
